ac518b5d8ea80eb9281fcc6ea8e8e831b5592229
MartinCvetkovic <martincvetkovic2000@yahoo.com>
2023-05-18 00:30:01 +0200

    Keygen finished

--- src/backend/keygen.py
+++ src/backend/keygen.py
@@ -1,2 +1,25 @@
-def generate():
-    return
+import rsa
+from Crypto.PublicKey import DSA
+
+
+def generate(algorithm, keyLength):
+    if algorithm == "rsa":
+        return generateRsaKey(keyLength)
+    elif algorithm == "dsa":
+        return generateDsaKey(keyLength)
+
+    return None, None
+
+
+def generateRsaKey(keyLength):
+    return rsa.newkeys(keyLength)
+
+
+def generateDsaKey(keyLength):
+    keyPair = DSA.generate(keyLength)
+    return keyPair.y, keyPair.x
+
+
+pubKey, privKey = generate("dsa", 2048)
+print(pubKey)
+print(privKey)
